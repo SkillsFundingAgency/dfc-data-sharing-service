e1f95867f1c4bdb6e44dbb45f4bb1f8f3eae57de
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-01-31 12:39:17 +0000

    Adding new shared models project

---
+++ DFC.DSS/DSS.Models/Notification.cs
@@ -0,0 +1,12 @@
+﻿namespace DSS.Models
+{
+    public class Notification
+    {
+        public string? id { get; set; } = Guid.NewGuid().ToString();
+        public Guid? CustomerId { get; set; }
+        public Uri? ResourceURL { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string? TouchpointId { get; set; }
+        public Guid CollectionId { get; set; }
+    }
+}
