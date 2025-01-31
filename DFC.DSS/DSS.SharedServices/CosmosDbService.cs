462bd7d877d119a5c08ed5a41678319a58373339
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-01-31 11:46:29 +0000

    Adding Cosmos NuGet into shared service

--- DFC.DSS/DSS.SharedServices/CosmosDbService.cs
+++ DFC.DSS/DSS.SharedServices/CosmosDbService.cs
@@ -1,9 +1,31 @@
 ﻿using DSS.Interfaces;
+using Microsoft.Azure.Cosmos;
 
 namespace DSS.SharedServices
 {
     public class CosmosDbService : ICosmosDbService
     {
+        private readonly CosmosClient _cosmosDbClient;
+        //private readonly ILogger<CosmosDbService> _logger;
 
+        public CosmosDbService(CosmosClient cosmosClient/*, ILogger<CosmosDbService> logger*/)
+        {
+            _cosmosDbClient = cosmosClient;
+            //_logger = logger;
+        }
+
+        /*public async Task<ItemResponse<T>> CreateNewDocument(string databaseName, string containerName, Notification newDocument)
+        {
+            _logger.LogInformation($"{nameof(CreateNewNotificationDocument)} function has been invoked");
+            Container cosmosDbContainer = _cosmosDbClient.GetContainer(databaseName, containerName);
+
+            _logger.LogInformation("Attempting to create new document in Cosmos DB");
+
+            ItemResponse<Notification> createRequestResponse = await cosmosDbContainer.CreateItemAsync(newDocument, PartitionKey.None);
+
+            _logger.LogInformation($"{nameof(CreateNewNotificationDocument)} function has finished invocation");
+
+            return createRequestResponse;
+        }*/
     }
 }
