070706377532616eaf113939befb93a92017fae8
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-02-03 11:04:22 +0000

    Subscription is now retrievable

---
+++ DFC.DSS/DSS.Models/Subscription.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace DSS.Models
+{
+    public class Subscription
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid? SubscriptionId { get; set; }
+
+        public Guid? CustomerId { get; set; }
+
+        public string TouchPointId { get; set; }
+
+        public bool? Subscribe { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public string LastModifiedBy { get; set; }
+    }
+}
