a31ea65b755054951e058ecd7480600f557353e6
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-02-03 11:57:02 +0000

    Webchats are now retrievable

---
+++ DFC.DSS/DSS.Models/Webchat.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace DSS.Models
+{
+    public class WebChat
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid? WebChatId { get; set; }
+
+        public Guid? CustomerId { get; set; }
+
+        public Guid? InteractionId { get; set; }
+
+        public string DigitalReference { get; set; }
+
+        public DateTime? WebChatStartDateandTime { get; set; }
+
+        public DateTime? WebChatEndDateandTime { get; set; }
+
+        public TimeSpan? WebChatDuration { get; set; }
+
+        public string WebChatNarrative { get; set; }
+
+        public bool? SentToCustomer { get; set; }
+
+        public DateTime? DateandTimeSentToCustomers { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public string LastModifiedTouchpointId { get; set; }
+    }
+}
--- DFC.DSS/DSS.WebChats/Function1.cs
+++ DFC.DSS/DSS.WebChats/Function1.cs
@@ -1,24 +1,41 @@
+using DSS.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Text.Json;
 
 namespace DSS.WebChats
 {
     public class Function1
     {
         private readonly ILogger<Function1> _logger;
+        private readonly ICosmosDbService _cosmos;
 
-        public Function1(ILogger<Function1> logger)
+        public Function1(ILogger<Function1> logger, ICosmosDbService cosmos)
         {
             _logger = logger;
+            _cosmos = cosmos;
         }
 
         [Function("Function1")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+
+            string databaseName = Environment.GetEnvironmentVariable("webchatsDatabaseName").ToString();
+            string containerName = Environment.GetEnvironmentVariable("webchatsContainerName").ToString();
+
+            ItemResponse<Models.WebChat> webchatObject = await _cosmos.GenericRetrieveDocument<Models.WebChat>(
+                req.Headers["WebchatId"].ToString(), databaseName, containerName
+            );
+
+            return new JsonResult(webchatObject.Resource, new JsonSerializerOptions())
+            {
+                StatusCode = (int)HttpStatusCode.OK
+            };
         }
     }
 }
