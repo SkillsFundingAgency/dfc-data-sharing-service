fc0d7a2ff9599921a6e8f7c7c4f59e0e6f33d45a
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-02-03 09:55:17 +0000

    Adviser Detail is now retrievable

---
+++ DFC.DSS/DSS.Models/AdviserDetail.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DSS.Models
+{
+    public class AdviserDetail
+    {
+        [Newtonsoft.Json.JsonProperty(PropertyName = "id")]
+        public Guid? AdviserDetailId { get; set; }
+
+        public string AdviserName { get; set; }
+
+        public string AdviserEmailAddress { get; set; }
+
+        public string AdviserContactNumber { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public string SubcontractorId { get; set; }
+
+        public string CreatedBy { get; set; }
+    }
+}
