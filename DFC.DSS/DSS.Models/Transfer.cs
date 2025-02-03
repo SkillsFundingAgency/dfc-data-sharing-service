879ad382d63fd8d3bf09171e6923f00ef10e498f
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-02-03 11:52:35 +0000

    Transfer is now retrievable

---
+++ DFC.DSS/DSS.Models/Transfer.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace DSS.Models
+{
+    public class Transfer
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid? TransferId { get; set; }
+
+        public Guid CustomerId { get; set; }
+
+        public Guid? InteractionId { get; set; }
+
+        public string OriginatingTouchpointId { get; set; }
+
+        public string TargetTouchpointId { get; set; }
+
+        public string Context { get; set; }
+
+        public DateTime? DateandTimeOfTransfer { get; set; }
+
+        public DateTime? DateandTimeofTransferAccepted { get; set; }
+
+        public DateTime? RequestedCallbackTime { get; set; }
+
+        public DateTime? ActualCallbackTime { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public string LastModifiedTouchpointId { get; set; }
+    }
+}
