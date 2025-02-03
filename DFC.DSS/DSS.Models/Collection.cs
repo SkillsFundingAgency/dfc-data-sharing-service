af23448f06b236ed8c0752676b881c323221c979
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-02-03 10:00:35 +0000

    Collections are now retrievable

---
+++ DFC.DSS/DSS.Models/Collection.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace DSS.Models
+{
+    public class Collection
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid CollectionId { get; set; }
+
+        public Uri CollectionReports { get; set; }
+
+        public string TouchPointId { get; set; }
+
+        public string Ukprn { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
