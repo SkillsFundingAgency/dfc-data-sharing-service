82c07b2b08e362ac9859e1000399fd8694fef472
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-02-03 10:59:23 +0000

    Session is now retrievable

---
+++ DFC.DSS/DSS.Models/Session.cs
@@ -0,0 +1,35 @@
+﻿using DSS.Models.Enums;
+using Newtonsoft.Json;
+
+namespace DSS.Models
+{
+    public class Session
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid? SessionId { get; set; }
+
+        public Guid? CustomerId { get; set; }
+
+        public Guid? InteractionId { get; set; }
+
+        public DateTime? DateandTimeOfSession { get; set; }
+
+        public string VenuePostCode { get; set; }
+
+        public bool? SessionAttended { get; set; }
+
+        public ReasonForNonAttendance? ReasonForNonAttendance { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public string LastModifiedTouchpointId { get; set; }
+
+        public string SubcontractorId { get; set; }
+
+        public decimal? Longitude { get; set; }
+
+        public decimal? Latitude { get; set; }
+
+        public string CreatedBy { get; set; }
+    }
+}
