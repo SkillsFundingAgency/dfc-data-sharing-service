999d0479ca9e8e19dbc99fdd14fa0a475e618a5a
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-02-03 10:30:09 +0000

    Goal is now retrievable

---
+++ DFC.DSS/DSS.Models/Enums/GoalStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace DSS.Models.Enums
+{
+    public enum GoalStatus
+    {
+        [Description("In progress")]
+        InProgress = 1,
+        
+        Achieved = 2,
+        
+        [Description("No longer relevant")]
+        NoLongerRelevant = 99
+    }
+}
---
+++ DFC.DSS/DSS.Models/Enums/GoalType.cs
@@ -0,0 +1,10 @@
+﻿namespace DSS.Models.Enums
+{
+    public enum GoalType
+    {
+        Skills = 1,
+        Work = 2,
+        Learning = 3,
+        Other = 99
+    }
+}
