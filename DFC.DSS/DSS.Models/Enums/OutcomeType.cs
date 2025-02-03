c4a217da5711921af29b865a77ed3e9008fb0e4b
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-02-03 10:53:37 +0000

    Outcome is now retrievable

---
+++ DFC.DSS/DSS.Models/Enums/OutcomeType.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace DSS.Models.Enums
+{
+    public enum OutcomeType
+    {
+        [Description("Customer Satisfaction")]
+        CustomerSatisfaction = 1,
+
+        [Description("Career Management")]
+        CareersManagement = 2,
+
+        [Description("Sustainable Employment")]
+        SustainableEmployment = 3,
+
+        [Description("Accredited Learning")]
+        AccreditedLearning = 4,
+
+        [Description("Career Progression")]
+        CareerProgression = 5
+    }
+}
---
+++ DFC.DSS/DSS.Models/Outcome.cs
@@ -0,0 +1,35 @@
+﻿using DSS.Models.Enums;
+using Newtonsoft.Json;
+
+namespace DSS.Models
+{
+    public class Outcome
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid? OutcomeId { get; set; }
+
+        public Guid? CustomerId { get; set; }
+
+        public Guid? ActionPlanId { get; set; }
+
+        public Guid? SessionId { get; set; }
+
+        public string SubcontractorId { get; set; }
+
+        public OutcomeType? OutcomeType { get; set; }
+
+        public DateTime? OutcomeClaimedDate { get; set; }
+
+        public DateTime? OutcomeEffectiveDate { get; set; }
+        
+        public bool? IsPriorityCustomer { get; set; }
+
+        public string TouchpointId { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public string LastModifiedTouchpointId { get; set; }
+
+        public string CreatedBy { get; set; }
+    }
+}
