caa8cccea2e9b264a18b25b3c8a7f5485b7a48fc
amjones99 <59894100+amjones99@users.noreply.github.com>
2025-05-30 12:51:45 +0100

    Add new methods to log service

--- DFC.DSS/DSS.SharedServices/LogService.cs
+++ DFC.DSS/DSS.SharedServices/LogService.cs
@@ -14,12 +14,21 @@ namespace DSS.SharedServices
 
         public void LogMethodExit(string nameOfMethod)
         {
-            _logger.LogInformation($"Method '{nameOfMethod}' has finished invocation");
+            _logger.LogInformation("Method '{nameOfMethod}' has finished invocation", nameOfMethod);
+        }
+        public void LogMethodInvocation(string nameOfMethod)
+        {
+            _logger.LogInformation("Method '{nameOfMethod}' has been invocation", nameOfMethod);
         }
 
-        public void LogFunctionExit(string nameOfFunction)
+        public void LogFunctionExit(string nameOfFunction, Guid correlationId)
         {
-            _logger.LogInformation($"Function '{nameOfFunction}' has finished invocation");
+            _logger.LogInformation("Function '{nameOfFunction}' has finished invocation. Correlation GUID: {CorrelationGuid}", nameOfFunction, correlationId);
+        }
+
+        public void LogFunctionInvocation(string nameOfFunction)
+        {
+            _logger.LogInformation("Function '{FunctionName}' has been invoked", nameOfFunction);
         }
     }
 }
